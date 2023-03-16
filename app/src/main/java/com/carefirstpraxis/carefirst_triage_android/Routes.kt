851718d7014e114eb3a67638b166551b0689ad9c
Nick Sophinos <nick@wdeanmedical.com>
2023-03-15 20:14:31 -0400

    work on login screen

---
+++ app/src/main/java/com/carefirstpraxis/carefirst_triage_android/Routes.kt
@@ -0,0 +1,9 @@
+package com.carefirstpraxis.carefirst_triage_android
+
+sealed class Routes(val route: String) {
+  object SignUp : Routes("SignUp")
+  object ForgotPassword : Routes("ForgotPassword")
+  object Login : Routes("Login")
+  object Dashboard : Routes("Dashboard")
+  object Home : Routes("Home")
+}
--- app/src/main/java/com/carefirstpraxis/carefirst_triage_android/Screens.kt
+++ app/src/main/java/com/carefirstpraxis/carefirst_triage_android/Screens.kt
@@ -3,4 +3,5 @@ package com.carefirstpraxis.carefirst_triage_android
 object Screens {
     const val Splash = "Splash"
     const val Dashboard = "Dashboard"
+    const val Login = "Login"
 }
